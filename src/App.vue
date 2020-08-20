62ed9e472c7d3e4d429cb34bcdac197a0db77fc3
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2020-08-20 03:32:21 +0100

    creating navbar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,61 @@
 <template>
   <div id="app">
+    <Header />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Poppins';
+  font-weight: 500;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  width: 1280px;
+  margin: 0 auto;
+}
+
+.cyan {
+  color: hsl(180, 66%, 49%);
+}
+
+.dark-violet {
+  color: hsl(257, 27%, 26%);
+}
+
+.red {
+  color: hsl(0, 87%, 67%);
+}
+
+.gray {
+  color: hsl(0, 0%, 75%)
+}
+
+.grayish-violet {
+  color: hsl(257, 7%, 63%);
+}
+
+.very-dark-blue {
+  color: hsl(255, 11%, 22%);
+}
+
+.very-dark-violet {
+  color: hsl(260, 8%, 14%);
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="header">
+    <div class="navbar">
+      <h4>Shortly</h4>
+      <ul class="nav-el">
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Resources</li>
+      </ul>
+    </div>
+    <div class="div-user">
+      <a class="login" href="#">Login</a>
+      <a class="signup" href="#">Sign Up</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  border: 1px solid black;
+  position: fixed;
+  top: 0;
+  width: 1280px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #ffffff;
+  z-index: 99;
+}
+
+.navbar {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+h4 {
+  color: hsl(260, 8%, 14%);
+  font-size: 24px;
+}
+
+.nav-el {
+  display: flex;
+  justify-content: flex-start;
+  color: hsl(257, 7%, 63%);
+  font-size: 16px
+}
+
+.nav-el li {
+  list-style-type: none;
+}
+
+.nav-el li:not(:last-child) {
+  margin-right: 15px;
+}
+
+.nav-el li:hover {
+  color: hsl(260, 8%, 14%);
+  cursor: pointer;
+}
+
+.div-user {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.div-user a {
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.login {
+  color: hsl(257, 7%, 63%);
+  margin-right: 15px;
+}
+
+.signup {
+  color: #FFF;
+  padding: 10px 25px;
+  border-radius: 25px;
+  background-color: hsl(180, 66%, 49%);
+}
+
+@media screen and (max-width: 768px) {
+  .nav-el,
+  .div-user {
+    display: none;
+  }
+}
+</style>
